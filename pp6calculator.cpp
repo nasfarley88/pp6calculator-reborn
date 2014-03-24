32def6901947b5d4c303579de3e1a3a20c6197d2
Nathanael Farley <nasfarley88@gmail.com>
2014-03-24 18:13:09 +0000

    Added quadratic solver function to program

--- pp6calculator.cpp
+++ pp6calculator.cpp
@@ -1,12 +1,66 @@
 #include <iostream>
 #include <string>
 #include <regex>
+// #include <cmath>
+#include <complex>
 
 double interceptXAxis(double m, double c) {
   ///
   /// A function to calculate the intercept on the x-axis of a straight line
   /// 
   return c/m;
+}
+
+struct quadraticRoots
+{
+  std::complex<double> root1,root2; 
+};
+
+std::ostream& operator<<(std::ostream& s, const quadraticRoots& c)
+{
+  ///
+  /// Be smart about what to print when it comes to roots of quadratic equations
+  /// 
+  if(c.root1 == c.root2 && c.root1.imag() != 0)
+    s << c.root1 << " (repeated root)";
+  else if(c.root1 == c.root2 && c.root1.imag() == 0)
+    s << c.root1.real() << " (repeated root)";
+  else if(c.root1.imag() == 0 || c.root2.imag() == 0)
+    s << c.root1.real() << " and " << c.root2.real();
+  else 				// all that's left is root1, root2 are complex
+    s << c.root1 << " and " << c.root2;
+  return s;
+}
+
+quadraticRoots quadraticSolver(double aReal, double bReal, double cReal)
+{
+  quadraticRoots roots = quadraticRoots();
+  roots.root1 = std::complex<double>(0,0);
+  roots.root2 = std::complex<double>(0,0);
+
+  std::complex<double> a = aReal;
+  std::complex<double> b = bReal;
+  std::complex<double> c = cReal;
+
+  if((b*b - 4.0*a*c) == std::complex<double>(0,0))
+    {
+      roots.root1 = (-b)/(2.0*a);
+      roots.root2 = roots.root1;
+    }
+  else
+    {
+      roots.root1 = (-b+sqrt(b*b-4.0*a*c))/(2.0*a);
+      roots.root2 = (-b-sqrt(b*b-4.0*a*c))/(2.0*a);
+    }
+    return roots;
+}
+
+void coutChosen(std::string x)
+{
+  ///
+  /// Function to cout "You have chosen the [x] function"
+  /// 
+  std::cout << "You have chosen the " << x << " function!" << std::endl;
 }
 
 int main(int argc, char *argv[])
@@ -23,17 +77,43 @@ int main(int argc, char *argv[])
 	    << "Please specify which function you would like to perform: "
 	    << std::endl
 	    << "> ";
-  std::cin >> decision;
-  if(std::regex_match(decision, std::regex("(^i$)|(intercept)")))
-    {
-     std::cout << "You have chosen the intercept function!" << std::endl;
-    }
-p
-  std::cout << "You chose " << decision << std::endl;
-  std::cout << "Sorry, I'm not yet complete enough to give you what you want."
-	    << std::endl
-	    << "Goodbye! I hope to see you again soon!"
-	    << std::endl;
   
+  std::getline(std::cin,decision);
+  
+  if(std::regex_match(decision, std::regex("(^i$)|(.*intercept.*)")))
+    {
+      // initialise variables for y=mx+c
+      double m,c;
+      
+      coutChosen("intercept");
+      std::cout << "Please specify a gradient (m): ";
+      std::cin >> m;
+      std::cout << "Please specify an intercept (c): " << std::endl;
+      std::cin >> c;
+      std::cout << "The x intercept for the line y = " << m << "x + " << c
+		<< " is " << interceptXAxis(m,c) << "." << std::endl;
+    }
+  else if(std::regex_match(decision, std::regex("(^q$)|(.*quadratic.*)")))
+    {
+      // Initialise variables for (-b±√(b^2 - 4ac))/(2a)
+      double a, b, c;
+
+      coutChosen("quadratic solver");
+      std::cout << "Choose the x^2 term (a): " << std::endl << "> ";
+      std::cin >> a;
+      std::cout << "Choose the x term (b): " << std::endl << "> ";
+      std::cin >> b;
+      std::cout << "Choose the constant term (c): " << std::endl << "> ";
+      std::cin >> c;
+
+      std::cout << "The roots for your chosen quadratic are  "
+		<< quadraticSolver(a,b,c) << std::endl;
+    }
+  else
+    {
+      std::cout << "You chose " << decision << std::endl;
+      std::cout << "Sorry, I'm not yet complete enough to give you what you want."
+		<< std::endl;
+    }
   return 0;
 }
