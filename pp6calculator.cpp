06d4a9c29c585f7929565806eb3befd07ecc4b72
Nathanael Farley <nasfarley88@gmail.com>
2014-03-25 17:30:58 +0000

    Put headers for FourVector and ThreeVector classes

--- pp6calculator.cpp
+++ pp6calculator.cpp
@@ -3,47 +3,13 @@
 #include <regex>
 // #include <cmath>
 #include <complex>
+#include "ThreeVector.hpp"
+#include "FourVector.hpp"
+
 
 // TODO put these in separate files when CMake is working
-class ThreeVector
-{
-private:
-  double x_,y_,z_;
-public:
-  ThreeVector()
-    : x_(0), y_(0), z_(0) {}
-  ThreeVector(double x, double y, double z)
-    : x_(x), y_(y), z_(z) {}
-  double getx() const { return x_; }
-  double gety() const { return y_; }
-  double getz() const { return z_; }
-  double setx(double x) { x_ = x; return x_; }
-  double sety(double y) { y_ = y; return y_; }
-  double setz(double z) { z_ = z; return z_; }
-  double modulus();
-};
 
 
-class FourVector
-{
-private:
-  double t_,x_,y_,z_;
-public:
-  FourVector()
-    : t_(0), x_(0), y_(0), z_(0) {}
-  FourVector(double t, double x, double y, double z)
-    : t_(t), x_(x), y_(y), z_(z) {}
-  double gett() const { return t_; }
-  double getx() const { return x_; }
-  double gety() const { return y_; }
-  double getz() const { return z_; }
-  ThreeVector getThreeVector() const { return ThreeVector(x_,y_,z_); }
-  double sett(double t) { t_ = t; return t_; }
-  double setx(double x) { x_ = x; return x_; }
-  double sety(double y) { y_ = y; return y_; }
-  double setz(double z) { z_ = z; return z_; }
-  double modulusSquared();
-};
 
 
 
---
+++ pp6libs/ThreeVector.hpp
@@ -0,0 +1,21 @@
+#ifndef THREEVECTOR_HPP
+#define THREEVECTOR_HPP
+class ThreeVector
+{
+private:
+  double x_,y_,z_;
+public:
+  ThreeVector()
+    : x_(0), y_(0), z_(0) {}
+  ThreeVector(double x, double y, double z)
+    : x_(x), y_(y), z_(z) {}
+  double getx() const { return x_; }
+  double gety() const { return y_; }
+  double getz() const { return z_; }
+  double setx(double x) { x_ = x; return x_; }
+  double sety(double y) { y_ = y; return y_; }
+  double setz(double z) { z_ = z; return z_; }
+  double modulus();
+};
+
+#endif
